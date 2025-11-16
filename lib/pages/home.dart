2454213ef4f361180173dec4855e69e49cbe36b5
jui1960 <juifull196044@gmail.com>
2025-11-16 21:25:55 +0600

    new

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -11,7 +11,45 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Container(
+      backgroundColor: Color(0xff703eff),
+      body: Container(
+        margin: EdgeInsets.only(top: 30.0,left: 20.0),
+        child: Column(children: [Row(children: [
+          Image.asset("images/wave.png",height:
+          40,width: 40,fit:BoxFit.cover,
+          ),
+
+          SizedBox(width: 10.0,),
+          Text(
+            "Hello,",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+
+          Text(
+            " Jannati",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Spacer(),
+          
+          Container(
+            padding: EdgeInsets.all(5),
+            margin: EdgeInsets.only(right: 20.0),
+
+            decoration: BoxDecoration(
+            color: Colors.white,borderRadius: BorderRadius.circular(30)),
+            child:Icon(Icons.person,color: Color(0xff703eff),size:40),
+          ),
+        ])]),
       ),
     );
   }
--- lib/pages/onboarding.dart
+++ lib/pages/onboarding.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class Onboarding extends StatefulWidget {
   const Onboarding({super.key});
 
@@ -13,45 +14,41 @@ class _OnboardingState extends State<Onboarding> {
       body: Container(
         child: Column(
           children: [
-
             Image.asset("images/onboard.png"),
-            SizedBox(height: 20.0,),
-            Padding(padding:
-            const EdgeInsets.only(left: 20.0,right: 20.0),),
+            SizedBox(height: 20.0),
+            Padding(padding: const EdgeInsets.only(left: 20.0, right: 20.0)),
+            Padding(padding: const EdgeInsets.only(left: 4000.0, right: 20.0)),
             Padding(
-              padding: const EdgeInsets.only(left: 4000.0,right: 20.0),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 20.0,right: 20.0),
+              padding: const EdgeInsets.only(left: 20.0, right: 20.0),
               child: Text(
-                  "Enjoy the new experience of chatting with global friend",
+                "Enjoy the new experience of chatting with global friend",
                 textAlign: TextAlign.center,
                 style: TextStyle(
-                    color: Colors.black,fontSize: 25,
-                    fontWeight: FontWeight.bold),
-
+                  color: Colors.black,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
 
-            SizedBox(height: 30.0),
+            SizedBox(height: 10.0),
+            Padding(padding: const EdgeInsets.only(left: 4000.0, right: 20.0)),
             Padding(
-              padding: const EdgeInsets.only(left: 4000.0,right: 20.0),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 20.0,right: 20.0),
+              padding: const EdgeInsets.only(left: 20.0, right: 20.0),
               child: Text(
                 "Connect people around the world for free",
                 textAlign: TextAlign.center,
                 style: TextStyle(
-                    color: Colors.black54,fontSize: 18,
-                    fontWeight: FontWeight.w500),
-
+                  color: Colors.black54,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                ),
               ),
             ),
-            SizedBox(height: 20.0),
+            SizedBox(height: 70.0),
 
             Container(
-              margin: EdgeInsets.only(left: 50.0,right: 70.0),
+              margin: EdgeInsets.only(left: 50.0, right: 70.0),
 
               child: Material(
                 elevation: 3.0,
@@ -60,46 +57,40 @@ class _OnboardingState extends State<Onboarding> {
                 child: Container(
                   height: 70,
 
-                  padding: EdgeInsets.only(top:8.0,left: 40.0,bottom: 8.0),
+                  padding: EdgeInsets.only(top: 8.0, left: 40.0, bottom: 8.0),
                   width: MediaQuery.of(context).size.width,
                   decoration: BoxDecoration(
-                    color: Color(0xff703eff),borderRadius: BorderRadius.circular(30),),
+                    color: Color(0xff703eff),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
                   child: Row(
                     // mainAxisAlignment: MainAxisAlignment.center,
-
                     children: [
+                      Image.asset(
+                        "images/google.png",
+                        height: 50,
+                        width: 50,
 
-                      Image.asset("images/google.png",
-                      height: 50,
-                      width: 50,
-
-
-                      fit: BoxFit.cover,
+                        fit: BoxFit.cover,
                       ),
-                      SizedBox(width:20,),
-                      Text("Sign in with Google",
-                      textAlign: TextAlign.center,
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 21,
-                            fontWeight: FontWeight.bold),
-                          ),
-
+                      SizedBox(width: 20),
+                      Text(
+                        "Sign in with Google",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 21,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
                     ],
                   ),
-
-
-
-                  ),
+                ),
               ),
             ),
-
-
           ],
         ),
       ),
     );
   }
 }
-
-
