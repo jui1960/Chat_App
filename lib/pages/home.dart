1440be73fd0f4795e1757f166dfe0b00319167c7
jui1960 <juifull196044@gmail.com>
2025-11-16 23:47:25 +0600

    update

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -13,43 +14,77 @@ class _HomeState extends State<Home> {
     return Scaffold(
       backgroundColor: Color(0xff703eff),
       body: Container(
-        margin: EdgeInsets.only(top: 30.0,left: 20.0),
-        child: Column(children: [Row(children: [
-          Image.asset("images/wave.png",height:
-          40,width: 40,fit:BoxFit.cover,
-          ),
+        margin: EdgeInsets.only(top: 40.0),
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [Row(children: [
+              Image.asset("images/wave.png",height:
+              50,width: 50,fit:BoxFit.cover,
+              ),
 
-          SizedBox(width: 10.0,),
-          Text(
-            "Hello,",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
+              SizedBox(width: 7.0,),
+              Text(
+                "Hello,",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
 
-          Text(
-            " Jannati",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          Spacer(),
-          
-          Container(
-            padding: EdgeInsets.all(5),
-            margin: EdgeInsets.only(right: 20.0),
+              Text(
+                " Jui",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Spacer(),
 
-            decoration: BoxDecoration(
-            color: Colors.white,borderRadius: BorderRadius.circular(30)),
-            child:Icon(Icons.person,color: Color(0xff703eff),size:40),
-          ),
-        ])]),
+              Container(
+                padding: EdgeInsets.all(5),
+                margin: EdgeInsets.only(right: 20.0),
+
+                decoration: BoxDecoration(
+                    color: Colors.white,borderRadius: BorderRadius.circular(30)),
+                child:Icon(Icons.person,color: Color(0xff703eff),size:40),
+              ),
+            ]),
+              Text(
+                "   Welcome to",
+                // textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Color.fromARGB(197,255,255,255),
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+
+                ),
+              ),
+
+
+              Text(
+                "  ChatUp",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(color: Colors.white),
+                child: Column(children: [
+                  Text("")
+                ],),
+              )
+
+
+            ]),
+
       ),
     );
   }
