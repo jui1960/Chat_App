88c4a2fed198b84b27e8c2b43a4fc7b6ae1e5810
jui1960 <juifull196044@gmail.com>
2025-11-17 17:23:24 +0600

    update

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -16,75 +15,81 @@ class _HomeState extends State<Home> {
       body: Container(
         margin: EdgeInsets.only(top: 40.0),
         child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [Row(children: [
-              Image.asset("images/wave.png",height:
-              50,width: 50,fit:BoxFit.cover,
-              ),
-
-              SizedBox(width: 8.0,),
-              Text(
-                "Hello,",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Image.asset(
+                  "images/wave.png",
+                  height: 50,
+                  width: 50,
+                  fit: BoxFit.cover,
                 ),
-              ),
 
-              Text(
-                " Jui",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
+                SizedBox(width: 1.0),
+                Text(
+                  "Hello,",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              Spacer(),
 
-              Container(
-                padding: EdgeInsets.all(5),
-                margin: EdgeInsets.only(right: 20.0),
-
-                decoration: BoxDecoration(
-                    color: Colors.white,borderRadius: BorderRadius.circular(30)),
-                child:Icon(Icons.person,color: Color(0xff703eff),size:40),
-              ),
-            ]),
-              Text(
-                "   Welcome to",
-                // textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Color.fromARGB(197,255,255,255),
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-
+                Text(
+                  " Jui",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
+                Spacer(),
 
+                Container(
+                  padding: EdgeInsets.all(5),
+                  margin: EdgeInsets.only(right: 20.0),
 
-              Text(
-                "  ChatUp",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 39.0,
-                  fontWeight: FontWeight.bold,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: Icon(Icons.person, color: Color(0xff703eff), size: 40),
                 ),
+              ],
+            ),
+            Text(
+              "   Welcome to",
+              // textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Color.fromARGB(197, 255, 255, 255),
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
               ),
-              Container(
+            ),
+
+            Text(
+              "  ChatUp",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 40.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20.0),
+
+            Expanded(
+              child: Container(
                 width: MediaQuery.of(context).size.width,
                 decoration: BoxDecoration(color: Colors.white),
-                child: Column(children: [
-                  Text("")
-                ],),
-              )
-
-
-            ]),
-
+                child: Column(children: [Text("")]),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
