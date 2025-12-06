caa8f141c0330da44b53595415ece01495126ee3
jui1960 <juifull196044@gmail.com>
2025-12-06 20:49:18 +0600

    firebase authentication

---
+++ lib/services/auth.dart
@@ -0,0 +1,56 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'database.dart';
+
+class AuthMethods{
+  final FirebaseAuth auth = FirebaseAuth.instance;
+
+  getCurrentUser()async{
+    return await auth.currentUser;
+  }
+  Future<void>SignInWithGoogle(BuildContext context) async{
+    final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+    final GoogleSignIn googleSignIn = GoogleSignIn();
+
+    final GoogleSignInAccount? googleSignInAccount =
+        await googleSignIn.signIn();
+
+    final GoogleSignInAuthentication? googleSignInAuthentication =
+        await googleSignInAccount?.authentication;
+
+    final AuthCredential credential = GoogleAuthProvider.credential(
+      idToken:  googleSignInAuthentication?.idToken,
+      accessToken: googleSignInAuthentication?.accessToken);
+
+
+    UserCredential result = await firebaseAuth.signInWithCredential(credential);
+    User? userDetails = result.user;
+
+    if(result != null){
+      Map<String, dynamic> userinfoMap={
+        "Name": userDetails!.displayName,
+        "Email": userDetails!.email,
+        "Image": userDetails.photoURL,
+        "Id": userDetails.uid,
+      };
+      await DatabaseMethods().addUser(userinfoMap,userDetails!.uid).then((value){
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+              backgroundColor: Colors.green,
+              content: Text("Registration Successfull!",
+          style:  TextStyle(
+            color:Colors.black,
+            fontSize: 22.0,fontWeight: FontWeight.bold,
+          ),))
+        );
+      });
+    }
+
+
+  }
+
+}
+
---
+++ lib/services/database.dart
@@ -0,0 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+
+class DatabaseMethods{
+  Future addUser(Map<String,dynamic>userInfoMap,String id)async{
+    return await FirebaseFirestore.instance
+        .collection("users").doc(id).set(userInfoMap);
+
+
+
+  }
+
+}
